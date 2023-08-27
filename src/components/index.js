3b96b82299e5a43459c8737c03b105a966caddf4
AlexParker82 <noyes.parker@gmail.com>
2023-08-27 18:47:40 -0400

    fxing imports

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,8 @@
 export { default as Navbar } from "./Navbar/Navbar"
+export { default as Searchbar } from "./Navbar/Searchbar"
 export { default as Feed } from "./Feed/Feed"
+export { default as Sidebar } from "./Feed/Sidebar"
+export { default as Videos } from "./Feed/Videos"
 export { default as ChannelDetail } from "./ChannelDetail"
 export { default as SearchFeed } from "./SearchFeed"
 export { default as VideoDetail } from "./VideoDetail"
