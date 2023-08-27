b778b2615033d18af1804e7647af4de244eb8125
AlexParker82 <noyes.parker@gmail.com>
2023-08-27 07:05:47 -0400

    start wortking on navbar and cleaning up imports

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { Stack } from "@mui/material";
+import { Link } from "react-router-dom";
 
-const Navbar = () => {
-	return <div>Navbar</div>;
-};
+const Navbar = () => (
+	<Stack>
+		<Link>1</Link>
+		<Link>2</Link>
+		<Link>3</Link>
+	</Stack>
+);
 
 export default Navbar;
